b9c7c93d997aff8ffc8cddb3411d43c25eda33dc
Parassahu2308 <sahujuparas786@gmail.com>
2023-04-11 19:23:18 +0530

    :lipstick: sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./App.css";
+import Home from "./pages/home/Home";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Topbar.css";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">ParasAdmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/9132551/pexels-photo-9132551.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
