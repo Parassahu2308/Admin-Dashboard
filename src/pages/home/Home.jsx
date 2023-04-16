af42e953f08e2404ace23738aaf300c55680fc5c
Parassahu2308 <sahujuparas786@gmail.com>
2023-04-16 12:54:45 +0530

    :lipstick: homepage completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./Home.css";
+import FeaturedInfo from "../../components/featureInfo/FeaturedInfo";
+import Chart from "../../components/chart/Chart";
+import { userData } from "../../dummyData.js";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Anayltics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
