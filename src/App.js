1378bd61ab65d75ec4ac5913f5e8e2f0cdd8f57b
Parassahu2308 <sahujuparas786@gmail.com>
2023-04-18 11:57:23 +0530

    :lipstick: user pages completed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import AddUser from "./pages/addUser/AddUser";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/users/:userId" element={<User />} />
+          <Route path="/newUser" element={<AddUser />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
